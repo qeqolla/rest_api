e38f70ead51a7fa0e4e6f38f518a0e9a658dcba7
qeqolla <andrikmit241@gmail.com>
2021-05-06 17:28:47 +0300

    added scopes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   belongs_to :city
+
+  scope :get_all_items_in_city, ->(city) { where(city: city) }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  scope :get_all_reviews, ->(user) { where(user: user) }
 end
