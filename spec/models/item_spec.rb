bf6a4c92b48aad18424dfcb8a5f654018c23bb91
qeqolla <andrikmit241@gmail.com>
2021-07-08 22:13:25 +0300

    tests now working with factory

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   describe ".get_all_items_in_city" do
-
-    let!(:city) { City.create(name: "test", country: "testtest") }
-    let!(:item) { Item.create(city: city, name: "city1") }
-    let!(:item1) { Item.create(city: City.create(name: "1111", country: "1111t1"), name: "city2") }
-    let!(:i1) { Item.create}
-
+    let!(:city) { create(:city, name: "test", country: "test_test") }
+    let!(:item) { create(:item, city: city, name: "city1") }
+    let!(:item1) { create(:item, city: city, name: "city2") }
     it 'should gets all items in city' do
-      expect(described_class.get_all_items_in_city(city)).to eq([item])
+      expect(described_class.get_all_items_in_city(city)).to eq([item, item1])
     end
   end
 end
+
+
+
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
+require 'support/factory_bot'
 
 RSpec.describe Review, type: :model do
   describe ".get_all_reviews" do
-    let!(:usr1) { User.create!(name: "test1", city: city) }
-    let!(:usr2) { User.create!(name: "test2", city: city) }
 
-    let!(:city) { City.create(name: "test", country: "testtest") }
-
+    let!(:user) { create(:user) }
+    let!(:city) { create(:city) }
+    let!(:booking) { create(:booking, user: user, item: item) }
+    let!(:rew1) { create(:review, feedback: "qqq", user: user, item: item) }
     let!(:item) { Item.create(city: city, name: "city1") }
 
-    let!(:rew1) { Review.create!(feedback: "qqq", user: usr1, item: item) }
-    let!(:rew2) { Review.create!(feedback: "222", user: usr2, item: item) }
 
     it 'gets all reviews by user1' do
-      expect(described_class.get_all_reviews(usr1)).to contain_exactly(rew1)
+      expect(described_class.get_all_reviews(user)).to contain_exactly(rew1)
+    end
+  end
+
+  describe ".check_if_record_with_user_exists" do
+    let!(:valid_user) { create(:user) }
+    let!(:invalid_user) { create(:user) }
+    let!(:city) { create(:city) }
+    let!(:item) { Item.create(city: city, name: "city1") }
+    let!(:valid_user_booking) { create(:booking, user: valid_user, item: item) }
+
+    it 'works correct' do
+      valid_review = Review.new(feedback: "qqq", user: valid_user, item: item)
+      expect(valid_review).to be_valid
+      invalid_review = Review.new(feedback: "qqq", user: invalid_user, item: item)
+      expect(invalid_review).to be_invalid
+      expect(invalid_review.errors.full_messages).to include("User never dealt with")
     end
   end
 end
